d2df1169dd7d17185a032769613033318d0046e8
redsnowc <nuanyang.44@gmail.com>
2019-11-24 23:39:27 +0800

    jinja2 模板练手

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 
+
+
+class User:
+    def __init__(self, permission, username):
+        self.permission = permission
+        self.username = username
+
+
+def hidden_tel(tel):
+    new_tel = tel.replace(tel[3:7], "*" * 4)
+    return new_tel
+
+
+app.add_template_filter(hidden_tel)
+
+
 @app.route('/')
 def index():
-    return "Hello World"
+    username = "redsnow"
+    languages = ["python", "javascript", "php", "java", "c", "c++"]
+    user = User('admin', 'redsnow')
+    tel = "13956781234"
+    return render_template("index.html", username=username, languages=languages, user=user, tel=tel)
+
+
+@app.route("/hi/<username>")
+def hi(username):
+    return "Hi %s" % username
+
+
+# @app.route("/go_home")
+# def go_home():
+#     return redirect(url_for('index'))
+
