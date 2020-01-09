c8451159d2d7350e4c700c86d8bcf706492e00c6
redsnowc <nuanyang.44@gmail.com>
2020-01-09 14:48:31 +0800

    重构 js 代码，解决样式错误

--- 04 博客首页/app/static/js/index.js
+++ 04 博客首页/app/static/js/index.js
@@ -1,30 +1,26 @@
-(function ($) {
-    const $jumbotron = $('#indexJumbotron');
-    const $toTop = $('#toTop');
-    const height = $(window).height();
+;(function ($) {
+    // 首页独有
 
-    // 打开网页时首页设置巨幕的高度
+    const $jumbotron = $('#indexJumbotron'); // 获取首页巨幕
+    const $window = $.globalVal.$window;     // 从 globalVal 取出 $window 对象
+    const height = $.globalVal.height;       // 从 globalVal 取出窗口高度
+    const $mainNavbar = $('#mainNavbar');    // 获取主导航栏
+
+    // 打开网页时首页设置巨幕的高度等于窗口高度
     $jumbotron.height(height);
 
     // 浏览器窗口尺寸改变时动态改变首页巨幕高度
-    $(window).on('resize', function () {
-        const height = $(window).height();
+    $window.on('resize', function () {
+        const height = $window.height();
         $jumbotron.height(height)
     });
 
-    // 根据滚动高度显示或隐藏导航栏背景、返回顶部按钮
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 100) {
-            $(".navbar").removeClass('navbar-transparent');
-            $toTop.removeClass('hide');
+    // 滚动高度超过 100 显示导航栏背景，反之隐藏
+    $window.scroll(function () {
+        if ($window.scrollTop() > 100) {
+            $mainNavbar.removeClass('navbar-transparent');
         } else {
-            $(".navbar").addClass('navbar-transparent');
-            $toTop.addClass('hide')
+            $mainNavbar.addClass('navbar-transparent');
         }
     });
-
-    // 返回页面顶部
-    $toTop.click(function () {
-        $.scrollAnimation(0)
-    })
 })(jQuery);
