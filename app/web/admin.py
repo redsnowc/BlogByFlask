6faa13775f9303b309bcb4411f1fb823c296f95f
redsnowc <nuanyang.44@gmail.com>
2020-02-01 00:45:45 +0800

    修正 meta 标签 url，重构 AJAX 请求校验函数

--- app/web/admin.py
+++ app/web/admin.py
@@ -58,12 +58,9 @@ def get_category():
 
     data = request.get_json()
 
-    result = check_ajax_request_data(data)
+    result = check_ajax_request_data(data, Category.__name__)
     if isinstance(result, str):
         return result
-
-    if result.__class__.__name__ != 'Category':
-        return json.dumps({'code': 0, 'msg': '指定查询模型与该查询不符'})
 
     successful_data = {'code': 1, 'data': {}}
     successful_data['data']['id'] = result.id
@@ -86,13 +83,9 @@ def update_category():
     form = EditCategoryForm()
     form_data = request.form
 
-    result = check_ajax_request_data(form_data)
-
+    result = check_ajax_request_data(form_data, Category.__name__)
     if isinstance(result, str):
         return result
-
-    if result.__class__.__name__ != 'Category':
-        return json.dumps({'code': 0, 'msg': '指定查询模型与该查询不符'})
 
     if form.validate_on_submit():
         with db.auto_commit():
@@ -146,12 +139,9 @@ def get_link():
 
     data = request.get_json()
 
-    result = check_ajax_request_data(data)
+    result = check_ajax_request_data(data, Link.__name__)
     if isinstance(result, str):
         return result
-
-    if result.__class__.__name__ != 'Link':
-        return json.dumps({'code': 0, 'msg': '指定查询模型与该查询不符'})
 
     successful_data = {'code': 1, 'data': {}}
     successful_data['data']['id'] = result.id
@@ -173,13 +163,10 @@ def update_link():
     form = EditLinkForm()
     form_data = request.form
 
-    result = check_ajax_request_data(form_data)
+    result = check_ajax_request_data(form_data, Link.__name__)
 
     if isinstance(result, str):
         return result
-
-    if result.__class__.__name__ != 'Link':
-        return json.dumps({'code': 0, 'msg': '指定查询模型与该查询不符'})
 
     if form.validate_on_submit():
         with db.auto_commit():
